811f27c245f7a10a2c10ba65bc7149ca7663f1a5
paulorobertoxavierjunior-create <paulorobertoxavierjunior@gmail.com>
2026-04-12 21:59:23 -0400

    Criar js/tabela-referencia.js

---
+++ js/tabela-referencia.js
@@ -0,0 +1,10 @@
+const TABELA_HAWKINS = [
+    { freq: 20, estado: "Vergonha", emocao: "Humilhação", cor: "#4A4A4A" },
+    { freq: 50, estado: "Apatia", emocao: "Desespero", cor: "#757575" },
+    { freq: 100, estado: "Medo", emocao: "Ansiedade", cor: "#B71C1C" },
+    { freq: 200, estado: "Coragem", emocao: "Afirmação", cor: "#FDD835" },
+    { freq: 500, estado: "Amor", emocao: "Reverência", cor: "#EC407A" },
+    { freq: 700, estado: "Iluminação", emocao: "Consciência", cor: "#FFFFFF" }
+];
+
+export default TABELA_HAWKINS;
